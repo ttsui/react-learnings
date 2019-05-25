3f0884ea0f773a7709be1cceb810030588d23539
Tony Tsui <tsui.tony@gmail.com>
2019-05-25 20:27:21 +1000

    Style business card to look nice

--- src/BusinessCard.tsx
+++ src/BusinessCard.tsx
@@ -1,26 +1,45 @@
 import React from "react";
-import { Address } from "./types";
+import { BusinessCardInfo } from "./types";
 
-interface BusinessCardProps {
-  address: Address;
-  phoneNumber?: string;
-}
-
-export const BusinessCard: React.FC<BusinessCardProps> = ({ address, phoneNumber }) => (
+export const BusinessCard: React.FC<BusinessCardInfo> = ({ address, phoneNumber }) => (
   <section
     style={{
       backgroundColor: "#fef5f0",
-      boxSizing: "border-box",
+      color: "darkgray",
+
+      fontFamily: "sans-serif",
+      fontSize: "13pt",
+      fontWeight: "bold",
+      fontVariant: "all-small-caps",
+
       display: "flex",
       flexDirection: "column",
       justifyContent: "space-between",
+
+      boxSizing: "border-box",
       padding: "30px",
       width: "530px",
       height: "317px"
     }}
   >
-    <h1>Business Card</h1>
-    <div
+    <section
+      style={{
+        display: "flex",
+        alignItems: "flex-start",
+        flexDirection: "column"
+      }}
+    >
+      <h1
+        style={{
+          color: "#59504b",
+          fontSize: "25pt",
+          marginBlockEnd: 0,
+          marginBlockStart: 0
+        }}
+      />
+    </section>
+
+    <section
       style={{
         display: "flex",
         alignItems: "flex-end",
@@ -29,12 +48,13 @@ export const BusinessCard: React.FC<BusinessCardProps> = ({ address, phoneNumber
         color: "darkgray",
 
         fontFamily: "sans-serif",
-        fontSize: "11pt",
+        fontSize: "13pt",
         fontWeight: "bold",
+        fontVariant: "all-small-caps"
       }}
     >
       <div>Call: {phoneNumber}</div>
       <div>Visit: {address.street.concat(address.suburb, address.state, String(address.postcode)).join(", ")}</div>
-    </div>
+    </section>
   </section>
 );
